39b09855ffbd1e848b930ec80f37644047acb38c
shamsmosowi <shams.mosowi@gmail.com>
2022-02-21 22:11:56 +0800

    updated rowy utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,65 @@
-export {url2storage} from './url2storage.js';
-export {getSecret} from './getSecret.js';
-export {getServiceAccountUser} from './getServiceAccountUser.js'
+import { getSecret } from "./secrets.js";
+import { getServiceAccountUser } from "./getServiceAccountUser.js";
+import { url2storage, data2storage } from "./storage";
+import { getNumericProjectId, getProjectId,getServiceAccountEmail } from "../metadataService.js";
+type RowyFile = {
+    downloadURL: string;
+    name: string;
+    type: string;
+    lastModifiedTS: number;
+  };
+  type RowyUser = {
+    email: any;
+    emailVerified: boolean;
+    displayName: string;
+    photoURL: string;
+    uid: string;
+    timestamp: number;
+  };
+  type uploadOptions = {
+    bucket?: string;
+    folderPath?: string;
+    fileName?: string;
+  };
+  interface Rowy {
+    metadata: {
+      projectId: () => Promise<string>;
+      projectNumber: () => Promise<string>;
+      serviceAccountEmail: () => Promise<string>;
+      serviceAccountUser: () => Promise<RowyUser>;
+    };
+    secrets: {
+      get: (name: string, version?: string) => Promise<string | undefined>;
+    };
+    storage: {
+      upload: {
+        url: (
+          url: string,
+          options: uploadOptions
+        ) => Promise<RowyFile | undefined>;
+        data: (
+          data: Buffer | string,
+          options: uploadOptions
+        ) => Promise<RowyFile | undefined>;
+      };
+    };
+  }
+
+const rowy: Rowy = {
+  metadata: {
+      projectId: getProjectId,
+      projectNumber:getNumericProjectId,
+      serviceAccountUser: getServiceAccountUser,
+      serviceAccountEmail: getServiceAccountEmail,
+  },
+  secrets: {
+    get: getSecret,
+  },
+  storage: {
+    upload: {
+      url: url2storage,
+      data: data2storage,
+    },
+  },
+};
+export default rowy;
